9e479dc32d63c1c6c0d19550df55ae866981b33d
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2024-10-22 01:02:36 +0600

    added proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-3xl bg-red-400'>Knowledge Cafe</h1>
+      <Header></Header>
+      <div className='md:flex'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
